0ee3d18ecb059a18ec40541c3afde9076ef0820f
Matheus Quevedo <matheusq10@gmail.com>
2024-07-19 19:37:26 -0300

    Criação: o conteúdo do componente foi criado

--- src/components/HomeLink/index.tsx
+++ src/components/HomeLink/index.tsx
@@ -1,3 +1,19 @@
-export function HomeLink() {
-    return ("")
+import { ButtonHTMLAttributes } from "react";
+import { Link } from "react-router-dom";
+
+interface HomeLinkProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    to: string;
+    icon: string;
+    iconAltText: string;
+    className: string;
+}
+
+export function HomeLink({ icon, to, className, iconAltText, ...props }: HomeLinkProps) {
+    return (
+        <Link to={to} className="w-28 h-28 bg-black">
+            <button {...props} className={className}>
+                <img src={icon} alt={iconAltText} />
+            </button>
+        </Link>
+    );
 }
