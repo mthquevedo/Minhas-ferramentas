2fe1d87dc8ca429bdefa07558a32f707448a0cbb
Matheus Quevedo <matheusq10@gmail.com>
2024-07-19 18:06:07 -0300

    Criação: a lógica de data foi criada para o cabeçalho ser dinâmico

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,40 @@
+import { DateTimeFormatOptions } from 'intl';
+import { useEffect, useState } from 'react';
+
 export function Header() {
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    useEffect(() => {
+        setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000)
+    }, []);
+
+    const options: DateTimeFormatOptions = { day: "numeric", month: "long" };
+    const formattedDate = currentDate.toLocaleDateString("pt-BR", options);
+    const hour: number = currentDate.getHours();
+    const minutes: number = currentDate.getMinutes();
+
+    function greeting(hour: number) {
+        if (hour > 4 && hour < 12) {
+            return "Bom dia";
+        } else if (hour >= 12 && hour < 18) {
+            return "Boa tarde";
+        } else {
+            return "Boa noite";
+        }
+    }
+
+    const currentGreeting = greeting(hour);
+
     return (
-        <header className="flex justify-between">
-            <p>Bom dia, 11 de julho</p>
-            <p>10:00</p>
+        <header className="flex justify-between text-sm">
+            <p><span className="font-medium">{currentGreeting}</span>, {formattedDate}</p>
+            <p className="border border-neutral-600 rounded-sm py-1 px-2">
+                {hour}
+                <span className="animate-blink">:</span>
+                {minutes < 10 ? `0${minutes}` : minutes}
+            </p>
         </header>
     );
 }
