55e334ec8728143339ce3d252655d14f500b8dec
Matheus Quevedo <matheusq10@gmail.com>
2024-07-18 22:33:47 -0300

    Deleção: o header e as estilizações foram removidas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,9 @@
 import { Link } from "react-router-dom";
-import { Header } from "../../components/Header";
 
 export function Home() {
 
     return (
-        <div className="w-100v h-100v bg-slate-500">
-            <Header />
-
+        <div className="">
             <main>
                 <Link to="contador"></Link>
             </main>
