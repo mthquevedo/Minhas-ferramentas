7f66aa60137b68239030e71eab223dd0332f6c52
Matheus Quevedo <matheusq10@gmail.com>
2024-07-18 22:35:46 -0300

    Ajuste: os estilos foram removidos

--- src/pages/Counter/index.tsx
+++ src/pages/Counter/index.tsx
@@ -4,7 +4,7 @@ export function Counter() {
     const [count, setCount] = useState(0)
 
     return (
-        <div className="w-100v h-100v bg-slate-800">
+        <div>
             <button onClick={() => setCount((count) => count + 1)}>
                 count is {count}
             </button>
