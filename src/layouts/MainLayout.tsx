251bba1e7e159c46c7fa1b151cbac27d5cbef6bf
Matheus Quevedo <matheusq10@gmail.com>
2024-07-19 18:51:51 -0300

    Criação: criação do footer + ajuste da flex box

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,11 +4,15 @@ import { Header } from "../components/Header";
 export function MainLayout() {
 
     return (
-        <div className="w-100v h-100v bg-gradient-to-b from-gray-50 to-blue-700/30 py-5 px-8">
+        <div className=" flex flex-col w-100v h-100v bg-gradient-to-b from-gray-50 to-blue-700/20 py-5 px-8">
             <Header />
 
             <Outlet />
 
+            <footer className="flex justify-between text-xs text-neutral-900 pt-3">
+                <p>© 2024</p>
+                <p>Desenvolvido por <a className="font-medium" href="http://" target="_blank">@Matheus Quevedo</a></p>
+            </footer>
         </div>
     )
 }
