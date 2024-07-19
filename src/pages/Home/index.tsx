8d8a384be2e9a948183cdd22044f6f750cce75b1
Matheus Quevedo <matheusq10@gmail.com>
2024-07-19 19:39:05 -0300

    Adição: o componente HomeLink foi adicionado a tela

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import { HomeLink } from "../../components/HomeLink";
+
 
 export function Home() {
 
     return (
-        <div className="flex-1">
-            <main>
-                <Link to="contador"></Link>
-            </main>
-        </div>
+        <main className="flex-1">
+            <div className="h-full flex flex-wrap justify-center gap-48 items-center">
+                <HomeLink to="" icon="" iconAltText="" className="" />
+                <HomeLink to="" icon="" iconAltText="" className="" />
+                <HomeLink to="" icon="" iconAltText="" className="" />
+            </div>
+        </main>
     )
 }
