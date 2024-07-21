b3f8d3de312136f0b1fbbb47cd15021233614779
Matheus Quevedo <matheusq10@gmail.com>
2024-07-21 18:56:19 -0300

    Adição: o componente para voltar foi adicionado

--- src/pages/Counter/index.tsx
+++ src/pages/Counter/index.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react"
+import { ReturnHomeButton } from "../../components/ReturnHomeButton"
 
 export function Counter() {
     const [count, setCount] = useState(0)
 
     return (
-        <div>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-        </div>
+        <main className="flex-1">
+
+            <ReturnHomeButton />
+
+            <div className="h-4/5 flex flex-wrap justify-center  items-center">
+
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </main>
     )
 }
