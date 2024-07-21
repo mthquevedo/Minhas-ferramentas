32c60f7416a935ed2cb96f9c4200c053f225ca32
Matheus Quevedo <matheusq10@gmail.com>
2024-07-21 18:23:05 -0300

    Adição: novas props e título foram adicionados

--- src/components/HomeLink/index.tsx
+++ src/components/HomeLink/index.tsx
@@ -1,19 +1,25 @@
 import { ButtonHTMLAttributes } from "react";
 import { Link } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
 
 interface HomeLinkProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     to: string;
-    icon: string;
-    iconAltText: string;
+    icon: React.ReactNode;
     className: string;
+    iconClassName?: string;
+    title: string;
 }
 
-export function HomeLink({ icon, to, className, iconAltText, ...props }: HomeLinkProps) {
+export function HomeLink({ icon, to, className, iconClassName, title, ...props }: HomeLinkProps) {
     return (
-        <Link to={to} className="w-28 h-28 bg-black">
-            <button {...props} className={className}>
-                <img src={icon} alt={iconAltText} />
-            </button>
-        </Link>
+        <div className="flex flex-col items-center gap-2">
+            <p className="font-medium">{title}</p>
+
+            <Link to={to}>
+                <button {...props} className={twMerge(" flex justify-center items-center w-24 h-24 rounded-lg shadow-lg transition-all duration-100	 hover:-translate-y-1", className)}>
+                    <p className={twMerge("text-4xl", iconClassName)}>{icon}</p>
+                </button>
+            </Link>
+        </div>
     );
 }
