774b467b69fce8e9b61a93c0783110100f1f6252
Matheus Quevedo <matheusq10@gmail.com>
2024-07-22 20:08:59 -0300

    Ajuste: o ícone foi atualizado

--- src/components/ReturnHomeButton/index.tsx
+++ src/components/ReturnHomeButton/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
-import { IoMdArrowBack } from "react-icons/io";
+import { TiArrowLeftThick } from "react-icons/ti";
 
 export function ReturnHomeButton() {
     return (
-        <button className="font-medium text-xs bg-neutral-800 text-gray-50 py-1 px-2 rounded-lg transition-all hover:shadow-lg">
+        <button className="font-medium text-xs bg-neutral-800 text-gray-50 py-1 px-2 rounded-lg transition-all hover:bg-neutral-700">
             <Link to="/" className="flex items-center gap-1">
-                <IoMdArrowBack />
+                <TiArrowLeftThick className="fill-yellow-400 text-lg" />
                 Voltar
             </Link>
         </button>
