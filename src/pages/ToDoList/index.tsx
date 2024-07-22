307f3e2a1724f81494a48c0d3f3d16133f0132f8
Matheus Quevedo <matheusq10@gmail.com>
2024-07-22 20:08:04 -0300

    Criação: o conteúdo do componente foi criado

--- src/pages/ToDoList/index.tsx
+++ src/pages/ToDoList/index.tsx
@@ -1,3 +1,25 @@
+import { ReturnHomeButton } from "../../components/ReturnHomeButton";
+
 export function ToDoList() {
-    return ("")
+    return (
+        <main className="flex-1">
+            <ReturnHomeButton />
+
+            <div className="h-4/5 flex justify-between items-start pt-10 mx-3">
+                <form action="" className="flex flex-col gap-5">
+                    <select name="" id="">
+                        <option value="">Selecione a categoria</option>
+                        <option value="Estudo">Estudo</option>
+                        <option value="Estudo">Trabalho</option>
+                        <option value="Estudo">Pessoal</option>
+                    </select>
+
+                    <input type="text" placeholder="Descreva a tarefa" />
+                    <button type="submit">Criar</button>
+                </form>
+
+                <div>teste</div>
+            </div>
+        </main>
+    )
 }
