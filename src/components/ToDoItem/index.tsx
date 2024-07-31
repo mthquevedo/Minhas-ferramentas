b2a976d0e93289ed66d53f607af53e161b953c45
Matheus Quevedo <matheusq10@gmail.com>
2024-07-30 21:50:54 -0300

    Criação: o componente foi criado

---
+++ src/components/ToDoItem/index.tsx
@@ -0,0 +1,29 @@
+import { GoTrash } from "react-icons/go";
+
+interface ToDoItemProps {
+    id: string;
+    genre: string;
+    content: string;
+    deleteAction: (id: string) => void;
+}
+
+export function ToDoItem({ genre, content, deleteAction, id }: ToDoItemProps) {
+
+
+    return (
+        <div className="bg-zinc-500/50 w-100 px-5 py-1 rounded-lg flex gap-8 items-center my-2">
+            <input type="checkbox" className="rounded-full w-6 h-6" />
+
+            <div className="w-full p-2 text-white overflow-hidden">
+                <p>{content}</p>
+            </div>
+
+            <div className="bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg">
+                {genre}
+            </div>
+
+
+            <button className="text-white " onClick={() => deleteAction(id)}><GoTrash /></button>
+        </div>
+    )
+}
