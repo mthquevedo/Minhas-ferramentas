150fb3d3c07b188138bd6cb3874bd0326ee09610
Matheus Quevedo <matheusq10@gmail.com>
2024-08-07 22:57:47 -0300

    Ajuste: a versão anterior foi comentada e uma nova utilizando o contexto está sendo desenvolvida

--- src/components/ToDoItem/index.tsx
+++ src/components/ToDoItem/index.tsx
@@ -4,10 +4,9 @@ interface ToDoItemProps {
     id: string;
     genre: string;
     content: string;
-    deleteAction: (id: string) => void;
 }
 
-export function ToDoItem({ genre, content, deleteAction, id }: ToDoItemProps) {
+export function ToDoItem({ genre, content }: ToDoItemProps) {
 
 
     return (
@@ -23,7 +22,34 @@ export function ToDoItem({ genre, content, deleteAction, id }: ToDoItemProps) {
             </div>
 
 
-            <button className="text-white " onClick={() => deleteAction(id)}><GoTrash /></button>
+            <button className="text-white "><GoTrash /></button>
         </div>
     )
 }
+// interface ToDoItemProps {
+//     id: string;
+//     genre: string;
+//     content: string;
+//     deleteAction: (id: string) => void;
+// }
+
+// export function ToDoItem({ genre, content, deleteAction, id }: ToDoItemProps) {
+
+
+//     return (
+//         <div className="bg-zinc-500/50 w-100 px-5 py-1 rounded-lg flex gap-8 items-center my-2">
+//             <input type="checkbox" className="rounded-full w-6 h-6" />
+
+//             <div className="w-full p-2 text-white overflow-hidden">
+//                 <p>{content}</p>
+//             </div>
+
+//             <div className="bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg">
+//                 {genre}
+//             </div>
+
+
+//             <button className="text-white " onClick={() => deleteAction(id)}><GoTrash /></button>
+//         </div>
+//     )
+// }
