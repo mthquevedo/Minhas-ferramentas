92c3370c9b10e6af9a88264b6ca700b941411520
Matheus Quevedo <matheusq10@gmail.com>
2024-08-08 00:25:14 -0300

    Ajuste: linhas vazias deletadas

--- src/components/ToDoItem/index.tsx
+++ src/components/ToDoItem/index.tsx
@@ -8,7 +8,6 @@ interface ToDoItemProps {
 
 export function ToDoItem({ genre, content }: ToDoItemProps) {
 
-
     return (
         <div className="bg-zinc-500/50 w-100 px-5 py-1 rounded-lg flex gap-8 items-center my-2">
             <input type="checkbox" className="rounded-full w-6 h-6" />
@@ -20,7 +19,6 @@ export function ToDoItem({ genre, content }: ToDoItemProps) {
             <div className="bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg">
                 {genre}
             </div>
-
 
             <button className="text-white "><GoTrash /></button>
         </div>
