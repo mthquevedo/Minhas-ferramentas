b2f562659eac2b5c4e6d636d52279caabf66d025
Matheus Quevedo <matheusq10@gmail.com>
2024-08-13 12:33:55 -0300

    Ajuste: a chamada para navegar entre as ferramentas foi ajustada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,11 +5,9 @@ import { HomeLink } from "../../components/HomeLink";
 export function Home() {
 
     return (
-        <main className="flex-1">
-
+        <main className="flex flex-col gap-10 flex-1 justify-center mb-36">
             <p className="text-center">Navegue pelas ferramentas abaixo:</p>
-
-            <div className="h-4/5 flex flex-wrap justify-center gap-48 items-center">
+            <div className=" flex flex-wrap justify-center gap-48 items-center">
                 <HomeLink title="Contador" to="contador" icon={<GoNumber />} iconClassName="text-amber-800" className="bg-sand" />
                 <HomeLink title="Lista de tarefas" to="lista-de-tarefas" icon={<IoListSharp />} iconClassName="text-orange-800" className="bg-orange-400" />
                 <HomeLink title="QR Code" to="gerador-de-qr-code" icon={<IoQrCodeOutline />} iconClassName="text-slate-300" className="bg-darkblue" />
