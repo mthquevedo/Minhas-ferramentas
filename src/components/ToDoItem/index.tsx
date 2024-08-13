d260a34d4cca9bb6a4affcaab4f385396c378f03
Matheus Quevedo <matheusq10@gmail.com>
2024-08-13 12:24:25 -0300

    Criação: a função de deletar a tarefa foi implementada

--- src/components/ToDoItem/index.tsx
+++ src/components/ToDoItem/index.tsx
@@ -1,12 +1,24 @@
 import { GoTrash } from "react-icons/go";
+import { useToDo } from "../../contexts/task";
+import { TaskProps } from "../../contexts/task";
+import { twMerge } from "tailwind-merge";
 
-interface ToDoItemProps {
-    id: string;
-    genre: string;
-    content: string;
-}
+export function ToDoItem({ id, genre, content }: TaskProps) {
+    const { deleteTask } = useToDo()
 
-export function ToDoItem({ genre, content }: ToDoItemProps) {
+    const genreColor = (genre: string) => {
+        if (genre === "Estudo") {
+            return "bg-orange-300";
+        } else if (genre === "Trabalho") {
+            return "bg-gray-300";
+        } else {
+            return "bg-lime-300";
+        }
+    }
+
+    const handleDeleteTask = (taskId: string) => {
+        deleteTask(taskId);
+    }
 
     return (
         <div className="bg-zinc-500/50 w-100 px-5 py-1 rounded-lg flex gap-8 items-center my-2">
@@ -16,38 +28,11 @@ export function ToDoItem({ genre, content }: ToDoItemProps) {
                 <p>{content}</p>
             </div>
 
-            <div className="bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg">
+            <div className={twMerge("bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg", genreColor(genre))}>
                 {genre}
             </div>
 
-            <button className="text-white "><GoTrash /></button>
+            <button className="text-white" onClick={() => handleDeleteTask(id)}><GoTrash /></button>
         </div>
     )
 }
-// interface ToDoItemProps {
-//     id: string;
-//     genre: string;
-//     content: string;
-//     deleteAction: (id: string) => void;
-// }
-
-// export function ToDoItem({ genre, content, deleteAction, id }: ToDoItemProps) {
-
-
-//     return (
-//         <div className="bg-zinc-500/50 w-100 px-5 py-1 rounded-lg flex gap-8 items-center my-2">
-//             <input type="checkbox" className="rounded-full w-6 h-6" />
-
-//             <div className="w-full p-2 text-white overflow-hidden">
-//                 <p>{content}</p>
-//             </div>
-
-//             <div className="bg-orange-300 w-20 py-1 px-1 text-center font-medium text-xs text-orange-950 border border-orange-950 rounded-lg">
-//                 {genre}
-//             </div>
-
-
-//             <button className="text-white " onClick={() => deleteAction(id)}><GoTrash /></button>
-//         </div>
-//     )
-// }
