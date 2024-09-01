46d165dc7f7ad82342750745052e1f4eff35447d
Matheus Quevedo <matheusq10@gmail.com>
2024-09-01 19:29:46 -0300

    Adição: novas configurações de responsividade foram adicionadas

--- src/pages/Counter/index.tsx
+++ src/pages/Counter/index.tsx
@@ -18,7 +18,7 @@ export function Counter() {
         <main className="flex-1">
             <ReturnHomeButton />
 
-            <div className="h-4/5 flex justify-center items-center gap-16">
+            <div className="h-4/5 flex justify-center items-center gap-16 md:hidden">
 
                 <div className="flex flex-col gap-6 pt-4">
                     <CounterButton icon={<RiArrowUpWideFill />} className="bg-green-500 hover:bg-green-400" onClick={increment} />
@@ -26,6 +26,16 @@ export function Counter() {
                 </div>
 
                 <p className="font-bold text-9xl text-center">{count}</p>
+            </div>
+
+            <div className="h-4/5 hidden flex-col justify-center items-center gap-16 md:flex">
+
+                <p className="font-bold text-9xl text-center">{count}</p>
+
+                <div className="flex flex-col gap-6 pt-4">
+                    <CounterButton icon={<RiArrowUpWideFill />} className="bg-green-500 hover:bg-green-400" onClick={increment} />
+                    <CounterButton icon={<RiArrowDownWideFill />} className="bg-red-500 hover:bg-red-400" onClick={decrement} />
+                </div>
             </div>
         </main>
     )
