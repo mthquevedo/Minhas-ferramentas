da1fb8be9e49195329c985df94b6c28e0483c4c9
Matheus Quevedo <matheusq10@gmail.com>
2024-09-01 19:29:36 -0300

    Adição: novas configurações de responsividade foram adicionadas

--- src/components/ToDoListContent/index.tsx
+++ src/components/ToDoListContent/index.tsx
@@ -40,15 +40,15 @@ export function ToDoListContent() {
         <div className="flex-1">
             <ReturnHomeButton />
 
-            <main className="h-5/6 bg-gray-400 flex justify-between items-start mt-7 shadow-lg rounded-lg mx-16">
+            <main className="h-5/6 bg-gray-400 flex md:flex-col justify-between items-start mt-7 shadow-lg rounded-lg mx-16 md:mx-1">
 
-                <aside className="w-30v h-full bg-gradient-to-t from-neutral-800 to-neutral-800/70 px-8 py-7 rounded-l-lg overflow-hidden">
+                <aside className="w-30v md:w-full h-full md:h-60v bg-gradient-to-t from-neutral-800 to-neutral-800/70 px-8 sm:px-5 py-7 md:pb-1 rounded-l-lg md:rounded-r-lg  overflow-hidden">
                     <form
                         className="flex flex-col justify-center gap-5"
                         onSubmit={handleCreateTask}
                     >
 
-                        <div className="flex items-center gap-4 pb-4">
+                        <div className="flex items-center gap-4 pb-4 sm:pb-0">
                             <span className="bg-orange-400 p-2 rounded-full"><IoListSharp /></span>
                             <h2 className="text-gray-50 font-bold text-lg">Crie uma nova tarefa:</h2>
                         </div>
@@ -76,17 +76,17 @@ export function ToDoListContent() {
                                 className="rounded-lg p-1 pl-2 bg-neutral-500 border border-gray-400 text-gray-50 placeholder:text-gray-50 focus:outline-none" />
                         </div>
 
-                        <div className="self-end pt-3">
+                        <div className="self-end pt-2 md:pt-0">
                             <button
                                 type="submit"
-                                className="bg-orange-400 text-orange-950 rounded-lg p-1 mt-2 font-medium w-28 border-2">
+                                className="bg-orange-400 text-orange-950 rounded-lg p-1 mt-2 sm:text-sm font-medium w-28 border-2">
                                 Adicionar
                             </button>
                         </div>
                     </form>
                 </aside>
 
-                <div className="w-80v h-full px-8 py-7 overflow-y-auto">
+                <div className="w-80v md:w-full h-full px-8 sm:px-5 py-7 md:py-4 overflow-y-auto">
                     <p className="font-medium">Tarefas:</p>
                     {tasks.map(task => {
                         return (
