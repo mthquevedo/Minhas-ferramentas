342a7e53f5045db69ec3f1a7bc9d278fef48deef
Matheus Quevedo <matheusq10@gmail.com>
2024-09-22 17:32:03 -0300

    Ajuste: mudança para tag mais semântica e adição de bg-cover

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,9 +2,8 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 
 export function MainLayout() {
-
     return (
-        <div className="flex flex-col gap-9 w-full h-100v bg-gradient-to-b from-gray-50 to-blue-700/20 py-5 px-8 overflow-auto">
+        <main className="flex flex-col gap-9 w-full h-100v bg-gradient-to-b from-gray-50 to-blue-700/20 bg-cover py-5 px-8 overflow-auto">
             <Header />
 
             <h1 className="flex justify-center items-center gap-2 text-center font-bold text-neutral-800 text-5xl sm:text-4xl py-2">
@@ -18,6 +17,6 @@ export function MainLayout() {
                 <p>© 2024</p>
                 <p>Criado por <a className="font-medium hover:underline" href="https://www.linkedin.com/in/matheus-quevedo-599315185/" target="_blank">@Matheus Quevedo</a></p>
             </footer>
-        </div>
+        </main>
     )
 }
