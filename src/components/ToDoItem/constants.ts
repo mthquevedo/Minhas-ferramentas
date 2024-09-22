e3295efe524f656ef44e7cf1c7e1dc3c88a19497
Matheus Quevedo <matheusq10@gmail.com>
2024-09-22 15:27:36 -0300

    Ajuste: foi criado um ajuste para utilizar um objeto constante

--- src/components/ToDoItem/constants.ts
+++ src/components/ToDoItem/constants.ts
@@ -1,5 +1,13 @@
-export enum GenreEnum {
-    ESTUDO = "Estudo",
-    TRABALHO = "Trabalho",
-    PESSOAL = "Pessoal",
-}
+// export enum GenreEnum {
+//     ESTUDO = "Estudo",
+//     TRABALHO = "Trabalho",
+//     PESSOAL = "Pessoal",
+// }
+
+export const taskGenre = {
+    ESTUDO: "Estudo",
+    TRABALHO: "Trabalho",
+    PESSOAL: "Pessoal",
+} as const;
+
+export type taskGenreValues = typeof taskGenre[keyof typeof taskGenre];
